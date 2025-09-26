591d216ba40ec93de57b66b45c53f4db0c39bf0a
Ersin Karaduman <ersin.karaduman@hva.nl>
2025-09-26 16:49:41 +0200

    work

---
+++ frontend/src/CorrelationHeatmap.tsx
@@ -0,0 +1,13 @@
+export default function CorrelationHeatmap() {
+  return (
+    <div className="w-full max-w-5xl bg-white shadow rounded p-3">
+      <img
+        src="/correlation_heatmap.png"
+        alt="Correlation matrix heatmap"
+        className="w-full h-auto"
+      />
+    </div>
+  )
+}
+
+
